553dd2109eaa88c315a7333e655a38f85be0dd37
jrimyak <imyakjake@gmail.com>
2020-03-29 19:20:41 -0500

    kth stat

--- QuadSum/QuadrupletSum.java
+++ QuadSum/QuadrupletSum.java
@@ -1,3 +1,5 @@
+package QuadSum;
+
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
--- kthOrder/kthOrder.java
+++ kthOrder/kthOrder.java
@@ -1,7 +1,9 @@
-package kthOrder;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
-import java.util.LinkedList;
-import java.util.*;
+
 public class kthOrder {
 
     public static class TreeNode {
@@ -15,13 +17,17 @@ public class kthOrder {
         }
     }
 
+    /**
+     * 
+     * @param root
+     * @param key
+     * @return
+     */
     public static TreeNode insert(TreeNode root, int key) {
         TreeNode curr = root;
         TreeNode parent = null;
-
-        if(root == null) {
-            return new TreeNode(key);
-        }
+        //TreeNode in = new TreeNode(key);
+       
 
         while(curr != null) {
             parent = curr;
@@ -34,16 +40,26 @@ public class kthOrder {
             }
         }
 
+        if(root == null) {
+            return new TreeNode(key);
+        }
+
         if( key < parent.val) {
-            parent = parent.left;
+            parent.left = new TreeNode(key);
         }
         else {
-            parent = parent.right;
+            parent.right = new TreeNode(key);
         }
         return root;
     }
 
-    public int kStatistic(TreeNode root, int k) {
+    /**
+     * 
+     * @param root
+     * @param k
+     * @return
+     */
+    public static int kStatistic(TreeNode root, int k) {
         while(root != null) {
             if(k == root.count + 1) {
                 return root.val;
@@ -57,6 +73,61 @@ public class kthOrder {
         return -1;
     }
     public static void main(String[] args) {
+        File file1 = new File("KthOrderSample/" + args[0]); 
+        File file2 = new File("KthOrderSample/" + args[1]);   
+        //uses scanner to read in the file
+         Scanner scanner = null;
+         Scanner scanner2 = null;
+         
+         try {
+             scanner = new Scanner(file1);
+           
+         } catch(FileNotFoundException e) {
+             System.out.println(e);
+         }
+         try {
+          
+            scanner2 = new Scanner(file2);
         
+        } catch(FileNotFoundException e) {
+            System.out.println(e);
+        }
+        
+         //adds the contents of the data file to an arraylsit 
+         ArrayList<Integer> data = new ArrayList<>();
+         while (scanner.hasNext()) {
+             if (scanner.hasNextInt()) {
+                 data.add(scanner.nextInt());
+             } 
+             else {
+                 scanner.next();
+             }
+         }
+
+         ArrayList<Integer> position = new ArrayList<>();
+         while(scanner2.hasNext()) {
+             if(scanner2.hasNextInt()) {
+                 position.add(scanner2.nextInt());
+             }
+             else {
+                 scanner2.next();
+             }
+         }
+
+         TreeNode root = null;
+         for(int key : data) {
+             root = insert(root, key);
+         }
+         int[] answers = new int[position.size()];
+         int i = 0;
+         for(int pos : position) {
+            answers[i] = kStatistic(root, pos);
+            i++;
+         }
+      //   System.out.println(position.get(0));
+         for(int j=0 ; j< i; j++) {
+             System.out.println("" + answers[j]);
+            
+         }
     }
 }
